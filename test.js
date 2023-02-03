adbda56eee81db1744aed4bdd93181657fc855ae
Buntin-Synthia <aokitakumi72@gmail.com>
2023-02-04 04:10:09 +0900

    **

---
+++ test.js
@@ -0,0 +1,8 @@
+const { postForDiscord } = require('./postForDiscord.js');
+
+postForDiscord(
+    'test',
+    'test',
+    'https://cdn.discordapp.com/attachments/1048616126046814218/1048617531415461969/nazuna.png',
+    '1070729349906583602'
+);
--- twitter.js
+++ twitter.js
@@ -1,35 +1,54 @@
 const fs = require('fs');
 const { TwitterApi } = require('twitter-api-v2');
 const config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
+const { postForDiscord } = require('./postForDiscord.js');
 
-const client = new TwitterApi(config.BearerToken);
+//const client = new TwitterApi(config.bearer_token);
+const client = new TwitterApi({
+    clientId: config.client_id,
+});
+async function lookupBookmarks() {
+    const bookmarks = await client.v2.bookmarks({
+        expansions: ['referenced_tweets.id'],
+    });
 
-async function searchUsers(query) {
-    const foundUsers = await client.v1.searchUsers(query);
+    for await (const bookmark of bookmarks) {
+        const quotedTweet = bookmarks.includes.quote(bookmark);
 
-    // use an async for-of to iterate over the multiple result pages!
-    for await (const user of foundUsers) {
-        console.log('User matching search:', user.screen_name);
+        if (quotedTweet) {
+            console.log(
+                'Bookmarked tweet',
+                bookmark.id,
+                'is a quote of',
+                quotedTweet.id
+            );
+        }
     }
 }
 
-async function getTimeLine() {
-    // Home timeline is available in v1 API, so use .v1 prefix
-    const homeTimeline = await client.v1.homeTimeline();
+async function tweet(content) {
+    const { data: createdTweet } = await client.v2.tweet(content);
+    console.log('Tweet', createdTweet.id, ':', createdTweet.text);
+}
 
-    // Current page is in homeTimeline.tweets
-    console.log(homeTimeline.tweets.length, 'fetched.');
-
-    const nextHomePage = await homeTimeline.next();
-    console.log(
-        'Fetched tweet IDs in next page:',
-        nextHomePage.tweets.map((tweet) => tweet.id_str)
+async function getOauth2Token() {
+    // Don't forget to specify 'offline.access' in scope list if you want to refresh your token later
+    const callbackurl = 'https://api.buntin.tech/twitter';
+    const { url, codeVerifier, state } = client.generateOAuth2AuthLink(
+        callbackurl,
+        {
+            scope: [
+                'tweet.read',
+                'users.read',
+                'bookmark.read',
+                'offline.access',
+            ],
+        }
     );
+    console.log('url:' + url);
+    console.log('codeVerifier:' + codeVerifier);
+    console.log('state:' + state);
+    // Redirect your user to {url}, store {state} and {codeVerifier} into a DB/Redis/memory after user redirection
 }
 
-async function whoami() {
-    const me = await client.v2.me();
-    console.log('My name is', me.name);
-}
-
-whoami();
+getOauth2Token();
