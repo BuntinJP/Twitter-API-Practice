40011bfa73497edc8be9af5abf120591309458da
Buntin-Synthia <aokitakumi72@gmail.com>
2023-02-09 05:18:58 +0900

    カオスすぎる式だけど動く

--- twitter.js
+++ twitter.js
@@ -11,6 +11,7 @@ const OutBookmarksDB = new Nedb({
     autoload: true,
 });
 const config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
+const { setTimeout } = require('timers/promises');
 const loading = require('loading-cli');
 
 //-----------------Twitter-----------------
@@ -28,62 +29,69 @@ const callbackurl = 'https://api.buntin.tech/twitter';
 //-----------------Functions-----------------
 
 const lookupBookmarks = async () => {
-    const load = loading('ブックマーク取得開始').start();
-    //max 800
-    const bookmarks = await client.v2.bookmarks({
-        expansions: [
-            'referenced_tweets.id',
-            'author_id',
-            'attachments.media_keys',
-        ],
-        'media.fields': [
-            'media_key',
-            'preview_image_url',
-            'type',
-            'url',
-            'public_metrics',
-            'non_public_metrics',
-            'organic_metrics',
-            'promoted_metrics',
-            'alt_text',
-            'variants',
-        ],
-    });
-    const loadFetch = loading('ブックマーク取得').start();
-    await bookmarks.fetchLast(1000);
-    loadFetch.succeed(bookmarks._realData.data.length + '件取得');
-    const users = bookmarks._realData.includes.users;
-    load.text = 'ユーザー取得中...';
-    userDB.insert(users, function (err, newDoc) {
-        // Needy debug
-    });
-    load.text = 'メディア取得中...';
-    const media = bookmarks._realData.includes.media;
-    mediaDB.insert(media, function (err, newDoc) {
-        // Needy debug
-    });
-    load.text = 'ブックマーク取得中...';
-    const c = bookmarks._realData.data.length;
-    let cnt = 1;
-    for await (const bookmark of bookmarks) {
-        load.text = 'ブックマーク取得中... (' + cnt + '/' + c + ')';
-        let insBM = {
-            id: bookmark.id,
-            text: bookmark.text,
-            author_id: bookmark.author_id,
-            attachments: bookmark.attachments,
-        };
-        bookmarkDB.insert(insBM, function (err, newDoc) {
+    await (async () => {
+        const load = loading('ブックマーク取得開始').start();
+        //max 800
+        const bookmarks = await client.v2.bookmarks({
+            expansions: [
+                'referenced_tweets.id',
+                'author_id',
+                'attachments.media_keys',
+            ],
+            'media.fields': [
+                'media_key',
+                'preview_image_url',
+                'type',
+                'url',
+                'public_metrics',
+                'non_public_metrics',
+                'organic_metrics',
+                'promoted_metrics',
+                'alt_text',
+                'variants',
+            ],
+        });
+        saveAsJson(bookmarks, 'bookmarks');
+        const loadFetch = loading('ブックマーク取得').start();
+        await bookmarks.fetchLast(1000);
+        loadFetch.succeed('1: ' + bookmarks._realData.data.length + '件取得');
+        const users = bookmarks._realData.includes.users;
+        load.text = 'ユーザー取得中...';
+        userDB.insert(users, function (err, newDoc) {
             // Needy debug
         });
-        bookmarkDB.insert(deleteDB, (err, nd) => {
-            //
+        await setTimeout(1000);
+        load.text = 'メディア取得中...';
+        const media = bookmarks._realData.includes.media;
+        mediaDB.insert(media, function (err, newDoc) {
+            // Needy debug
         });
-    }
-    load.succeed('ブックマーク取得完了');
+        await setTimeout(1000);
+        load.text = 'ブックマーク取得中...';
+        const c = bookmarks._realData.data.length;
+        let cnt = 1;
+        for await (const bookmark of bookmarks) {
+            load.text = 'ブックマーク取得中... (' + cnt + '/' + c + ')';
+            let insBM = {
+                id: bookmark.id,
+                text: bookmark.text,
+                author_id: bookmark.author_id,
+                attachments: bookmark.attachments,
+            };
+            bookmarkDB.insert(insBM, function (err, newDoc) {
+                // Needy debug
+            });
+            bookmarkDB.insert(deleteDB, (err, nd) => {
+                //
+            });
+        }
+        load.succeed('1: ブックマーク取得完了');
+        return;
+    })();
+    return;
 };
 //lookupBookmarks();
-
+//TODO: lookupBookmarksを、saveAsJsonのように、内部のNedb関連のインスタンスをグローバル変数としてではなく、引数として受け取るようにする
 const saveAsJson = (data, filename) => {
     fs.writeFileSync(
         `./JSON/${filename}.json`,
@@ -91,6 +99,252 @@ const saveAsJson = (data, filename) => {
         'utf8'
     );
 };
+
+const updateBookmarks = async () => {
+    await (async () => {
+        bookmarkDB.loadDatabase();
+        await bookmarkDB.find({}, async (err, docs) => {
+            if (err !== null) {
+                console.error(err);
+            }
+            const ids = docs.map((docs) => docs.id);
+            const load = loading('取得中...').start();
+            await getTweetsWithTimeBounce(ids, 0, bookmarkDB2, load).then(
+                () => {
+                    return;
+                }
+            );
+        });
+        return;
+    })();
+};
+
+const detaSave = (prevDB, finalDB) => {
+    let loadingDetaSave = loading('最終データベースに登録中...').start();
+    prevDB.loadDatabase();
+    prevDB.find({}, function (err, docs) {
+        if (err !== null) {
+            console.error(err);
+        }
+        finalDB.insert(docs, (err, newDoc) => {
+            if (err !== null) {
+                loadingDetaSave.fail('最終データベースに登録失敗');
+                console.error(err);
+            }
+            loadingDetaSave.succeed(
+                '3: 最終データベースに登録完了(' + newDoc.length + '件)'
+            );
+        });
+    });
+};
+
+/* const unBookmark = async (db) => {
+    db.loadDatabase();
+    db.find({}, function (err, docs) {
+        if (err !== null) {
+            console.error(err);
+        }
+        const ids = docs.map((docs) => docs.id);
+        const load = loading('ブックマーク削除中...').start();
+        unBookmarkWithTimeBounce(ids, 0, load);
+    });
+}; */
+const unBookmark = async (db, db2) => {
+    const loadUnBookmark = loading('ブックマーク削除開始').start();
+    db.loadDatabase();
+    await db.find({}, async (err, docs) => {
+        if (err !== null) {
+            console.error(err);
+        }
+        const ids = docs.map((docs) => docs.id);
+        let count = 0;
+        let cnt = 1;
+        for (const id of ids) {
+            loadUnBookmark.text =
+                'ブックマーク削除中... (' + cnt + '/' + ids.length + ')';
+            if (count >= 50) {
+                await sleepLoading(15, loadUnBookmark);
+                refreshBearerToken();
+                count = 0;
+            }
+            while (true) {
+                try {
+                    let result = await client.v2.deleteBookmark(id);
+                    if (result.data.bookmarked === false) {
+                    } else {
+                        let l = loading('削除失敗(' + id + ')').warn();
+                    }
+                    cnt++;
+                    count++;
+                    break;
+                } catch (e) {
+                    count = 0;
+                    await sleepLoading(15, loadUnBookmark);
+                    await refreshBearerToken();
+                }
+            }
+        }
+        loadUnBookmark.succeed('4: ブックマーク削除完了');
+    });
+    let loadDeleteDB = loading('DB削除中...').start();
+    let a = 0;
+    let b = 0;
+    db.remove({}, { multi: true }, function (err, num1) {
+        if (err !== null) {
+            console.error(err);
+        }
+        db2.remove({}, { multi: true }, function (err, num2) {
+            if (err !== null) {
+                console.error(err);
+            }
+            loadDeleteDB.succeed(
+                '4: DB削除完了(' + num1 + '件,' + num2 + '件)'
+            );
+        });
+    });
+};
+
+const unBookmark2 = async (db, db2) => {
+    const loadUnBookmark = loading('ブックマーク削除開始').start();
+    db.loadDatabase();
+    await db.find({}, async (err, docs) => {
+        const ids = docs.map((docs) => docs.id);
+        let count = 0;
+        let cnt = 1;
+        for (const id of ids) {
+            loadUnBookmark.text =
+                'ブックマーク削除中... (' + cnt + '/' + ids.length + ')';
+            if (count >= 50) {
+                await sleepLoading(15, loadUnBookmark);
+                refreshBearerToken();
+                count = 0;
+            }
+            while (true) {
+                try {
+                    let result = await client.v2.deleteBookmark(id);
+                    if (result.data.bookmarked === false) {
+                    } else {
+                        let l = loading('削除失敗(' + id + ')').warn();
+                    }
+                    cnt++;
+                    count++;
+                    break;
+                } catch (e) {
+                    count = 0;
+                    await sleepLoading(15, loadUnBookmark);
+                    await refreshBearerToken();
+                }
+            }
+        }
+        let loadDeleteDB = loading('DB削除中...').start();
+        db.remove({}, { multi: true }, function (err, num1) {
+            if (err !== null) {
+                console.error(err);
+            }
+            db2.remove({}, { multi: true }, function (err, num2) {
+                if (err !== null) {
+                    console.error(err);
+                }
+                loadDeleteDB.succeed(
+                    '4: DB削除完了(' + num1 + '件,' + num2 + '件)'
+                );
+                db.loadDatabase();
+                db2.loadDatabase();
+            });
+        });
+        loadUnBookmark.succeed('4: ブックマーク削除完了');
+    });
+};
+
+const refreshBearerToken = async () => {
+    const subClient = new TwitterApi({
+        clientId: config.client_id,
+        clientSecret: config.client_secret,
+    });
+    const {
+        client: refreshed,
+        accessToken,
+        refreshToken: newRefreshToken,
+    } = await subClient.refreshOAuth2Token(refreshToken);
+    bearerToken = accessToken;
+    refreshToken = newRefreshToken;
+    client = new TwitterApi(bearerToken);
+    fs.writeFileSync(
+        './config2.json',
+        JSON.stringify({
+            access_token: bearerToken,
+            refresh_token: refreshToken,
+        }),
+        'utf8'
+    );
+};
+
+const main = async () => {
+    await lookupBookmarks().then(() => {
+        //
+    });
+    await updateBookmarks();
+    detaSave(bookmarkDB2, OutBookmarksDB);
+    unBookmark2(bookmarkDB, bookmarkDB2);
+};
+
+//-----------------Main-------------------
+main();
+//-----------------CommonLevelFunctions-----------------
+/* const unBookmarkWithTimeBounce = async (ids, start, load = null) => {
+    let unit = 45;
+    let end = start + unit;
+    if (end > ids.length) {
+        end = ids.length;
+    }
+    let count = 0;
+    //削除処理
+    for (i of ids.slice(start, end)) {
+        load.text = '削除中...' + (start + count) + '/' + ids.length + ' ';
+        let commonLoading = loading('削除(' + i + ')');
+        commonLoading.frame(['←', '↖', '↑', '↗', '→', '↘', '↓', '↙']);
+        commonLoading.start();
+        while (true) {
+            try {
+                let result = await client.v2.deleteBookmark(i);
+                if (result.data.bookmarked === false) {
+                    commonLoading.succeed();
+                    break;
+                } else {
+                    commonLoading.fail();
+                }
+            } catch (e) {
+                commonLoading.fail(e);
+                load.text =
+                    '削除中...' +
+                    (start + count) +
+                    '/' +
+                    ids.length +
+                    '\n レート制限により一時停止中(10分) : (' +
+                    start +
+                    '/ ' +
+                    ids.length +
+                    ')';
+                await sleepLoading(10, load);
+            }
+        }
+    }
+    if (end < ids.length) {
+        load.text =
+            'レート制限により一時停止中(15分) : (' +
+            end +
+            '/ ' +
+            ids.length +
+            ')';
+        await sleep(900000).then(() => {
+            refreshBearerToken().then(() => {
+                unBookmarkWithTimeBounce(ids, end, load);
+            });
+        });
+    } else {
+        load.succeed('全てのブックマークを削除しました');
+    }
+}; */
 
 const test = async () => {
     const testDB = new Nedb({ filename: 'db/test.db', autoload: true });
@@ -132,7 +386,7 @@ const getTweets = async (ids) => {
     return rt;
 };
 
-const getTweetsWithTimeBounce = async (ids, start, load = null) => {
+const getTweetsWithTimeBounce = async (ids, start, db, load = null) => {
     //ids:Array of tweet id[String](MAX is infinty)
     //using recursion to avoid rate limit
     //betgin, start = 0;
@@ -142,167 +396,57 @@ const getTweetsWithTimeBounce = async (ids, start, load = null) => {
     }
     await getTweets(ids.slice(start, end)).then((tweets) => {
         let data = tweets.data;
-        bookmarkDB2.insert(data, (err, newDoc) => {
+        db.insert(data, (err, newDoc) => {
             //
         });
     });
     if (end < ids.length) {
         load.text = '取得中...' + end + '/' + ids.length;
-        sleep(1000).then(() => {
-            getTweetsWithTimeBounce(ids, end, load);
+        await sleep(1000).then(() => {
+            getTweetsWithTimeBounce(ids, end, db, load);
         });
     } else {
-        load.succeed('取得完了');
-        console.log('取得長:' + ids.length);
+        load.succeed('3: 取得完了' + '(取得長:' + ids.length + ')');
     }
 };
 
-const updateBookmarks = async () => {
-    bookmarkDB.loadDatabase();
-    bookmarkDB.find({}, function (err, docs) {
-        if (err !== null) {
-            console.error(err);
-        }
-        const ids = docs.map((docs) => docs.id);
-        const load = loading('取得中...').start();
-        getTweetsWithTimeBounce(ids, 0, load);
-    });
+/* const sleep = async (ms) => {
+    //
+    await setTimeout(ms);
 };
-
-const unBookmarkWithTimeBounce = async (ids, start, load = null) => {
-    let unit = 45;
-    let end = start + unit;
-    if (end > ids.length) {
-        end = ids.length;
-    }
-    let count = 0;
-    //削除処理
-    for (i of ids.slice(start, end)) {
-        load.text = '削除中...' + (start + count) + '/' + ids.length + ' ';
-        let commonLoading = loading('削除(' + i + ')');
-        commonLoading.frame(['←', '↖', '↑', '↗', '→', '↘', '↓', '↙']);
-        commonLoading.start();
-        while (true) {
-            try {
-                let result = await client.v2.deleteBookmark(i);
-                if (result.data.bookmarked === false) {
-                    commonLoading.succeed();
-                    break;
-                } else {
-                    commonLoading.fail();
-                }
-            } catch (e) {
-                commonLoading.fail(e);
-                load.text =
-                    '削除中...' +
-                    (start + count) +
-                    '/' +
-                    ids.length +
-                    '\n レート制限により一時停止中(10分) : (' +
-                    start +
-                    '/ ' +
-                    ids.length +
-                    ')';
-                await sleepLoading(600000, load);
-            }
+const sleepLoading = async (minutes, load) => {
+    let m = minutes;
+    load.text = 'レート制限により一時停止中...(残り' + m + '分)';
+    while (true) {
+        await setTimeout(60000);
+        m--;
+        load.text = 'レート制限により一時停止中...(残り' + m + '分)';
+        if (m <= 0) {
+            load.text = '待機終了';
+            break;
         }
     }
-    if (end < ids.length) {
-        load.text =
-            'レート制限により一時停止中(15分) : (' +
-            end +
-            '/ ' +
-            ids.length +
-            ')';
-        sleep(900000).then(() => {
-            refreshBearerToken().then(() => {
-                unBookmarkWithTimeBounce(ids, end, load);
-            });
-        });
-    } else {
-        load.succeed('全てのブックマークを削除しました');
-        console.log('削除長:' + ids.length);
-    }
+}; */
+const sleep = async (ms) => {
+    //
+    await setTimeout(ms);
+    return;
 };
 
-const unBookmark = async (ids) => {
-    bookmarkDB2.loadDatabase();
-    bookmarkDB2.find({}, function (err, docs) {
-        if (err !== null) {
-            console.error(err);
+const sleepLoading = async (minutes, load) => {
+    let m = minutes;
+    load.text = 'レート制限により一時停止中...(残り' + m + '分)';
+    while (true) {
+        await setTimeout(60000);
+        m--;
+        load.text = 'レート制限により一時停止中...(残り' + m + '分)';
+        if (m <= 0) {
+            load.text = '待機終了';
+            break;
         }
-        const ids = docs.map((docs) => docs.id);
-        const load = loading('ブックマーク削除中...').start();
-        unBookmarkWithTimeBounce(ids, 0, load);
-    });
-};
-
-const sleep = (ms) => {
-    //using for recursion
-    return new Promise((resolve) => setTimeout(resolve, ms));
-};
-
-const sleepLoading = (ms, load) => {
-    //using for recursion
-    load.text = 'レート制限回避のため、一時停止中(' + ms / 60000 + '分)...';
-    return new Promise((resolve) => setTimeout(resolve, ms));
-};
-const refreshBearerToken = async () => {
-    const subClient = new TwitterApi({
-        clientId: config.client_id,
-        clientSecret: config.client_secret,
-    });
-    const {
-        client: refreshed,
-        accessToken,
-        refreshToken: newRefreshToken,
-    } = await subClient.refreshOAuth2Token(refreshToken);
-    bearerToken = accessToken;
-    refreshToken = newRefreshToken;
-    client = new TwitterApi(bearerToken);
-    fs.writeFileSync(
-        './config2.json',
-        JSON.stringify({
-            access_token: bearerToken,
-            refresh_token: refreshToken,
-        }),
-        'utf8'
-    );
-};
-
-const detaSave = () => {
-    const load = loading('データベース初期化...').start();
-    let ins = 0;
-    let del = 0;
-    bookmarkDB2
-        .find({})
-        .sort({ created_at: -1 })
-        .exec((err, docs) => {
-            OutBookmarksDB.insert(docs, (err, doc) => {
-                console.log('完了');
-            });
-        });
-    OutBookmarksDB.loadDatabase();
-    let Out = [];
-    OutBookmarksDB.find({})
-        .sort({ created_at: -1 })
-        .exec((err, docs) => {
-            Out = docs;
-        });
-    OutBookmarksDB.remove({}, { multi: true }, (err, numRemoved) => {
-        del = numRemoved;
-    });
-    load.text = 'データベース挿入中...';
-    OutBookmarksDB.insert(Out, (err, doc) => {
-        ins = doc.length;
-    });
-    if (ins == del) {
-        load.succeed('データベース保存完了(' + ins + '件)');
-    } else {
-        load.fail('データベース保存失敗(' + ins + '件/' + del + '件)');
     }
+    return;
 };
-
 const moveTheData = (from, to) => {
     from.find({}, (err, docs) => {
         to.insert(docs, (err, doc) => {
@@ -315,6 +459,7 @@ const clearTheData = (from) => {
     from.remove({}, { multi: true }, (err, numRemoved) => {
         console.log('完了\n' + numRemoved + '件削除');
     });
+    from.loadDatabase();
 };
 
 const sortTheData = (from) => {
@@ -340,8 +485,6 @@ const sortTheData = (from) => {
         load.fail('ソート失敗 データの整合性が失われた可能性があります。');
     }
 };
-//-----------------Main-----------------
-sortTheData(OutBookmarksDB);
 //-----------------OAuth2-----------------
 async function getOauth2Redirect() {
     // Don't forget to specify 'offline.access' in scope list if you want to refresh your token later
